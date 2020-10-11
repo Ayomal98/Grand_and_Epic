c1e96b4298758b528b58a35828440966acc99235
Ayomal Praveen <ayomalpraveen33@gmail.com>
2020-10-12 00:09:41 +0530

    added the signup.php & content slider for the home page

--- Hotel_Website/connect_login.php
+++ Hotel_Website/connect_login.php
@@ -1,10 +1,11 @@
 <? php
+
     if(isset($_POST['Submit-btn'])){
         if($_POST['email']=='ayomalpraveen33@gmail.com' && $_POST['password']=='abcd1234'){
-            echo "successfully logged in";
-            header("location:../index.php");
+            session_start();
+            header("location:www.google.com");
         }else{
-            header("location:#")
+            header("location:#");
         }
     }
 ?>
--- Javascript/script.js
+++ Javascript/script.js
@@ -6,6 +6,10 @@ function closeSlideMenu() {
   document.getElementById("side-nav").style.width = "0px";
   document.getElementById("header-container").style.marginLeft = "0";
 }
+// document.getElementById("nav-svg").addEventListener("click", function () {
+//   document.getElementById("side-nav").style.width = "250px";
+//   document.getElementById("header-container").style.marginLeft = "250px";
+// });
 document.getElementById("btn-login").addEventListener("click", function () {
   document.querySelector(".bg-modal").style.display = "flex";
 });
