4b089157ba7e036024a456d85aa5b6973e264852
Ayomal Praveen <ayomalpraveen33@gmail.com>
2020-10-13 02:03:29 +0530

    made minor changes regarding styles

--- Hotel_Website/connect_login.php
+++ Hotel_Website/connect_login.php
@@ -6,19 +6,20 @@ if (isset($_POST['Submit'])) {
     $usertype = $_POST['User-Type'];
     $query = '';
     if ($usertype == 'Employee') {
-        $query = "SELECT Email,Password,User_Role FROM employee WHERE Email='" . $email . "' AND Password='" . $password . "'";
+        $query = "SELECT First_Name,Email,Password,User_Role FROM employee WHERE Email='" . $email . "' AND Password='" . $password . "'";
         $result = mysqli_query($con, $query);
         if (mysqli_num_rows($result) == 1) {
             $row = mysqli_fetch_assoc($result);
             if ($row["User_Role"] == "Admin") {
-                header('Location:../Admin/AdminDashboard.html');
-                $_SESSION["First_Name"] = $row["First_Name"];
+                $_SESSION['username'] = $row['First_Name'];
+                header('Location:../Admin/AdminDashboard.php');
             } elseif ($row["User_Role"] == "Hotel Manager") {
-                header('Location:../Hotel_Manager/HotelManagerDashboard.html');
+                $_SESSION['username'] = $row['First_Name'];
+                header('Location:../Hotel_Manager/HotelManagerDashboard.php');
                 $_SESSION["First_Name"] = $row["First_Name"];
             } elseif ($row["User_Role"] == "Hotel Supervisor") {
-                header('Location:../Hotel_Supervisor/SupervisorDashboard.html');
-                $_SESSION["First_Name"] = $row["First_Name"];
+                $_SESSION['username'] = $row['First_Name'];
+                header('Location:../Hotel_Supervisor/SupervisorDashboard.php');
             }
         }
     } else {
