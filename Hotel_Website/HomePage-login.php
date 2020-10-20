35f4fb1444fd8bd6917b37a73521015157ce1e17
Ayomal Praveen <ayomalpraveen33@gmail.com>
2020-10-21 01:26:57 +0530

    added the logout file

--- Hotel_Website/HomePage-login.php
+++ Hotel_Website/HomePage-login.php
@@ -19,8 +19,15 @@ $username = $_SESSION['First_Name'];
 
         <?php include("side-nav-login.php"); ?>
 
-        <span class="fa fa-user" style="position:absolute;top:20px;right:40px;font-size:60px;color:white"></span>
-        <br><span style="position:absolute;top:100px;right:40px;font-size:20px;color:white"><?php echo "Logged in as $username"; ?></span>
+        <span class="fa fa-user" style="position:absolute;top:20px;right:40px;font-size:60px;color:white"><span class="far fa-caret-square-down" style="font-size:20px;margin-left:10px;" onclick="funcUserDetails()"></span></span>
+        <!--<br><span style="position:absolute;top:100px;right:40px;font-size:20px;color:white"></span>-->
+        <div id="user-detail-container">
+            <span class="fa fa-window-close" style="margin-left:130px;" onclick="funcCloseUserDetails()"></span>
+            <p style="margin-bottom: 10px;"><?php echo "Logged in as $username"; ?></P>
+            <hr style="color:teal">
+            <a href="logout.php"><input type="button" value="Log-out" name="logout-btn" style="margin-top:5px;margin-left:85px;padding:5px;background-color:black;color:white;border-radius:5px;"></a>
+
+        </div>
         <div class="text-container">
             <span class="text1">Grand &</span>
             <span class="text2">Epic
@@ -78,6 +85,15 @@ $username = $_SESSION['First_Name'];
     <?php include("footer-footer.php"); ?>
     <script src="../Javascript/script.js"></script>
     <script src="../Javascript/sticky-nav.js"></script>
+    <script>
+        function funcUserDetails() {
+            document.getElementById('user-detail-container').style.display = "block";
+        }
+
+        function funcCloseUserDetails() {
+            document.getElementById('user-detail-container').style.display = "none";
+        }
+    </script>
 </body>
 
 </html>
