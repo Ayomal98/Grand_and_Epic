3227c739c19105509ed0551750280a9504fd5373
hasinihatharasinghe97 <hasinividushanka@gmail.com>
2020-11-16 11:53:22 +0530

    SupervisorManageMeals.php file added

--- Hotel_Website/connect_login.php
+++ Hotel_Website/connect_login.php
@@ -4,7 +4,6 @@ if (isset($_POST['Submit'])) {
     $email = mysqli_real_escape_string($con, $_POST['email']);
     $password = mysqli_real_escape_string($con, $_POST['password']);
     $usertype = mysqli_real_escape_string($con, $_POST['User-Type']);
-    $password_hashed = password_hash($_POST['password'], PASSWORD_DEFAULT);
     $query = '';
     if ($usertype == 'Employee') {
         $query = "SELECT First_Name,Email,Password,User_Role FROM employee WHERE Email='" . $email . "' AND Password='" . $password . "'";
@@ -13,14 +12,21 @@ if (isset($_POST['Submit'])) {
             $row = mysqli_fetch_assoc($result);
             if ($row["User_Role"] == "Admin") {
                 $_SESSION['username'] = $row['First_Name'];
+                $_SESSION["User_Email"] = $row["Email"];
                 header('Location:../Admin/AdminDashboard.php');
             } elseif ($row["User_Role"] == "Hotel Manager") {
                 $_SESSION['username'] = $row['First_Name'];
-                header('Location:../Hotel_Manager/HotelManagerDashboard.html');
+                $_SESSION["User_Email"] = $row["Email"];
+                header('Location:../Hotel_Manager/HotelManagerDashboard.php');
                 $_SESSION["First_Name"] = $row["First_Name"];
             } elseif ($row["User_Role"] == "Hotel Supervisor") {
                 $_SESSION['username'] = $row['First_Name'];
+                $_SESSION["User_Email"] = $row["Email"];
                 header('Location:../Hotel_Supervisor/SupervisorDashboard.php');
+            } elseif ($row["User_Role"] == "Employee") {
+                $_SESSION['username'] = $row['First_Name'];
+                $_SESSION["User_Email"] = $row["Email"];
+                header('Location:../Employees/EmployeeDashboard.php');
             }
         }
     } else {
